119c70e053ef180774cc5a031f040faf144b6310
Madelyn Luansing <mluansing@captechventures.com>
2018-11-29 11:34:57 -0500

    updated UI

--- app/src/main/java/com/mluansing/phonecalltimer/MainActivity.java
+++ app/src/main/java/com/mluansing/phonecalltimer/MainActivity.java
@@ -3,22 +3,35 @@ package com.mluansing.phonecalltimer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.mluansing.phonecalltimer.util.SharedPreferenceUtil;
 
 public class MainActivity extends AppCompatActivity {
 
-    SwitchCompat switchTimerMode;
     CountdownFragment countdownFragment;
     TimerFragment timerFragment;
+
+    // view
+    SwitchCompat switchTimerMode;
+    TextView modeDescription, labelTimer, labelCountdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeView();
         initializeTimerMode();
+    }
+
+    private void initializeView() {
+        modeDescription = findViewById(R.id.mode_description);
+        switchTimerMode = findViewById(R.id.switch_timer_mode);
+        labelCountdown = findViewById(R.id.label_countdown);
+        labelTimer = findViewById(R.id.label_timer);
     }
 
     /**
@@ -29,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         changeTimerMode(isTimer);
 
-        switchTimerMode = findViewById(R.id.switch_timer_mode);
         switchTimerMode.setChecked(isTimer);
 
         switchTimerMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -50,9 +62,17 @@ public class MainActivity extends AppCompatActivity {
         if (isTimer) {
             timerFragment = new TimerFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_content, timerFragment).commit();
+            modeDescription.setText(R.string.mode_timer_description);
+            modeDescription.setTextColor(getResources().getColor(R.color.colorAccent));
+            labelCountdown.setVisibility(View.INVISIBLE);
+            labelTimer.setVisibility(View.VISIBLE);
         } else {
             countdownFragment = new CountdownFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_content, countdownFragment).commit();
+            modeDescription.setText(R.string.mode_countdown_description);
+            modeDescription.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            labelTimer.setVisibility(View.INVISIBLE);
+            labelCountdown.setVisibility(View.VISIBLE);
         }
     }
 }
